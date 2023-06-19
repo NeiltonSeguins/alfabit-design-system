b55ec36bb274459782b38127d3937c88817fc5a4
NeiltonSeguins <seguins.neilton@gmailcom>
2023-06-19 15:59:25 -0300

    Vídeo 1.4 - Adicionando estilos e documentando

---
+++ components/Typography/Typography.stories.ts
@@ -0,0 +1,22 @@
+// Typography.stories.ts
+import { Meta, StoryObj } from "@storybook/react";
+
+import Typography, { type TypographyProps } from "./Typography";
+
+const meta: Meta<TypographyProps> = {
+  title: "Atoms/Typography",
+  component: Typography,
+  argTypes: {
+    className: {
+      type: "string",
+    },
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<TypographyProps> = {
+  args: {
+    children: "Texto",
+  },
+};
--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 
-export type TypographProps = {
-  variant?: string;
-  size?: string;
+export type TypographyProps = {
+  variant?: "primary" | "secondary" | "tertiary";
+  size?: "xs" | "sm" | "md" | "xl" | "title1" | "title2" | "title3";
   children: React.ReactNode;
   element?: keyof JSX.IntrinsicElements;
+} & JSX.IntrinsicElements["p"];
+
+const variantClassMap = {
+  variant: {
+    primary: "text-gray-primary",
+    secondary: "text-gray-secondary",
+    tertiary: "text-gray-tertiary",
+  },
+  size: {
+    xs: "text-xs",
+    sm: "text-sm",
+    md: "text-md",
+    xl: "text-xl",
+    title1: "text-txl",
+    title2: "text-tlg",
+    title3: "text-tmd",
+  },
 };
 
-const Typograph = ({
-  variant = "",
-  size = "",
+const Typography = ({
+  variant = "primary",
+  size = "md",
   children,
   element = "p",
-}: TypographProps) => {
-  const Element = element;
-  return <Element>{children}</Element>;
+  ...rest
+}: TypographyProps) => {
+  const Element = element as any;
+  return (
+    <Element
+      className={`text-gray-primary text-sm font-normal 
+  ${variantClassMap.variant[variant]} ${variantClassMap.size[size]}`}
+      {...rest}
+    >
+      {children}
+    </Element>
+  );
 };
 
-export default Typograph;
+export default Typography;
