9219d044ec7621612b1a5e5f1c9ec0239dc38038
NeiltonSeguins <seguins.neilton@gmailcom>
2023-06-20 16:01:24 -0300

    Vídeo 2.2 - Utilizando o classnames

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 
 export type DividerProps = {
   width?: string;
@@ -7,18 +8,28 @@ export type DividerProps = {
   children?: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 
+const colorClassMap = {
+  light: "bg-light",
+  dark: "bg-dark",
+  black: "bg-black",
+};
+
 const Divider = ({
   width,
-  height,
+  height = "h-[1px]",
   children,
-  bgColor,
+  bgColor = "black",
   ...rest
 }: DividerProps) => {
+  const colorClass = colorClassMap[bgColor];
+
+  const barClass = classNames(children ? "w-1/3" : "w-1/2", height, colorClass);
+
   return (
-    <div>
-      <div></div>
-      {children && <div>{children}</div>}
-      <div></div>
+    <div className={classNames(width, "flex items-center justify-center")}>
+      <div className={barClass}></div>
+      {children && <div className="px-3">{children}</div>}
+      <div className={barClass}></div>
     </div>
   );
 };
