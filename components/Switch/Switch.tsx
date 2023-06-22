a09d885815b0f5379c842247e2697ebc109dd808
NeiltonSeguins <seguins.neilton@gmailcom>
2023-06-22 15:28:26 -0300

    Video 4.2

--- components/Switch/Switch.tsx
+++ components/Switch/Switch.tsx
@@ -2,6 +2,8 @@ import {
   Switch as HeadlessSwitch,
   type SwitchProps as HeadlessSwitchProps,
 } from "@headlessui/react";
+import { useState } from "react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 export type SwitchProps = {
   defaultEnable?: boolean;
@@ -15,8 +17,32 @@ const Switch = ({
   variant = "common",
   disabled,
   onChange,
+  ...rest
 }: SwitchProps) => {
-  return <div></div>;
+  const [enabled, setEnabled] = useState(enabledByDefault);
+
+  const toggle = () => {
+    const newState = !enabled;
+    setEnabled(newState);
+    onChange?.(newState);
+  };
+
+  return (
+    <HeadlessSwitch
+      checked={enabled}
+      onChange={toggle}
+      disabled={disabled}
+      {...rest}
+    >
+      {variant === "common" && <span />}
+      {variant === "contract" && (
+        <span>
+          {enabled && <CheckIcon aria-disabled={disabled} />}
+          {enabled || <XMarkIcon aria-disabled={disabled} />}
+        </span>
+      )}
+    </HeadlessSwitch>
+  );
 };
 
 export default Switch;
