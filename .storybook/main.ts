1213c2e6981882ed7be9cb54b60444f01dac7579
NeiltonSeguins <seguins.neilton@gmailcom>
2023-06-23 15:24:58 -0300

    Video 5.3

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../components/**/*.mdx", "../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  staticDirs: ["../public"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
